a0b62536b48a40986370599ce51c6ef1aa8e35bd
Ramly23 <88806353+Ramly23@users.noreply.github.com>
2021-11-22 19:11:05 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const hamburger = document.querySelector('#hamburger');
+const close = document.querySelector('#close')
+const header = document.querySelector('header');
+const title = document.querySelector('#title');
+const mobileLinks = document.querySelector('.mobile-links');
+
+hamburger.addEventListener('click', function(){
+    if(header.classList.contains('open')) {
+        header.classList.remove('open');
+    }
+    else {
+        header.classList.add('open');
+        hamburger.classList.add('hidden');
+        close.classList.remove('hidden');
+        title.classList.add('hidden');
+        header.style.background = "linear-gradient(rgb(0,0,0), rgb(0,0,0)), url(images/mobile/image-hero.jpg)";
+        header.style.backgroundSize = "cover";
+        mobileLinks.classList.remove('hidden');
+    }
+});
+
+close.addEventListener('click', function(){
+    if(header.classList.contains('open')) {
+        header.classList.remove('open');
+        hamburger.classList.remove('hidden');
+        close.classList.add('hidden');
+        title.classList.remove('hidden');
+        header.style.background = "linear-gradient(rgba(4,9,30,0.5), rgba(4,9,30,0.5)), url(images/mobile/image-hero.jpg)";
+        mobileLinks.classList.add('hidden');
+    }
+    else {
+        header.classList.add('open');
+    }
+});
